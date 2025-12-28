6fab37a38bb2f292bf21a0c1958fd00bcac71fe7
HassanJahangir-2k8 <hassanjahangir2002@gmail.com>
2025-12-28 06:40:10 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+function calculateBMI() {
+    const weight = document.getElementById('weight').value;
+    const height = document.getElementById('height').value;
+    if(weight && height){
+        const bmi = (weight / ((height/100) ** 2)).toFixed(2);
+        document.getElementById('bmi-result').innerText = "Your BMI is: " + bmi;
+    } else {
+        document.getElementById('bmi-result').innerText = "Please enter valid numbers!";
+    }
+}
+
+
+
+
+
+
+
+
+
+const infoIcon = document.getElementById('info-icon');
+const infoTooltip = document.getElementById('info-tooltip');
+
+infoIcon.addEventListener('click', () => {
+    if(infoTooltip.style.display === 'block') {
+        infoTooltip.style.display = 'none';
+    } else {
+        infoTooltip.style.display = 'block';
+    }
+});
